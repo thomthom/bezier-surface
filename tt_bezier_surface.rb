5a595b37a9c0495fc36e2a198946c4932f64e7ec
Thomas Thomassen <thomas@thomthom.net>
2011-03-30 15:12:37 +0200

    Forgot to save all files at last commit.

--- tt_bezier_surface.rb
+++ tt_bezier_surface.rb
@@ -164,32 +164,6 @@ module TT::Plugins::BezierSurfaceTools
     
     return nil
   end
-
-  # Initates the tool to draw a new QuadPatch.
-  #
-  # @return [Boolean]
-  # @since 1.0.0
-  def self.draw_quadpatch
-    Sketchup.active_model.select_tool( nil )
-    Sketchup.active_model.tools.push_tool( CreatePatchTool.new )
-  end
-  
-  
-  # Initates the tool to draw a new QuadPatch.
-  #
-  # @return [Boolean]
-  # @since 1.0.0
-  def self.add_quadpatch
-    model = Sketchup.active_model
-    editor = self.get_editor( model )
-    return false unless editor
-    return false unless editor.active?
-    return false unless editor.selection.size == 1
-    edge = editor.selection[0]
-    return false unless edge.is_a?( BezierEdge )
-    edge.extrude_quad_patch
-    true
-  end
   
   
   # Get Instructor Path
