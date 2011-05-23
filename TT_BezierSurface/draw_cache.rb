e4c142528a90691f0c05532ca2bb9a8f1a393ae9
Thomas Thomassen <thomas@thomthom.net>
2011-05-23 13:00:03 +0200

    Changed: DrawCache relays all non-cached method to Sketchup::View for easy drop-in replacement.

--- TT_BezierSurface/draw_cache.rb
+++ TT_BezierSurface/draw_cache.rb
@@ -7,23 +7,48 @@
 
 
 module TT::Plugins::BezierSurfaceTools
-
+  
+  # Caches drawing instructions so complex calculations for generating the
+  # GL data can be reused.
+  #
+  # Redirect all Skethcup::View commands to a DrawCache object and call
+  # #render in a Tool's #draw event.
+  #
+  # @since 1.0.0
   class DrawCache
+    # (?) Move to TT_Lib ?
     
-    def initialize
+    # @param [Sketchup::View]
+    #
+    # @since 1.0.0
+    def initialize( view )
+      @view = view
       @commands = []
     end
     
+    # Clears the cache. All drawing instructions are removed.
+    #
+    # @return [Nil]
+    # @since 1.0.0
     def clear
       @commands.clear
+      nil
     end
     
-    def render( view )
+    # Draws the cached drawing instructions.
+    #
+    # @return [Sketchup::View]
+    # @since 1.0.0
+    def render
+      view = @view
       for command in @commands
         view.send( *command )
       end
+      view
     end
     
+    # Cache drawing commands and data. These methods received the finsihed
+    # processed drawing data that will be executed when #render is called.
     [
       :draw,
       :draw2d,
@@ -42,6 +67,20 @@ module TT::Plugins::BezierSurfaceTools
         @commands.size
       }
     }
+    
+    # Pass through methods to Sketchup::View so that the drawing cache object
+    # can easily replace Sketchup::View objects in existing codes.
+    #
+    # @since 1.0.0
+    def method_missing( *args )
+      view = @view
+      method = args.first
+      if view.respond_to?( method )
+        view.send(*args)
+      else
+        raise NoMethodError, "undefined method `#{method}' for #{self.class.name}"
+      end
+    end
     
     # @return [String]
     # @since 1.0.0
